0c21663ffb844a73d8e7b0e5b9ed430db48d2c01
czetao <czt_user@czt.com>
2019-08-31 10:43:30 +0800

    高并发 关于线程池的实现

--- src/main/java/Thread/ThreadPool/ThreadPool.java
+++ src/main/java/Thread/ThreadPool/ThreadPool.java
@@ -3,7 +3,7 @@ package Thread.ThreadPool;
 /**
  * @Auther: 陈泽涛
  * @Date: 2019/8/28 21:04
- * @Description: 简单的线程池定义
+ * @Description: 简单的线程池定义接口
  */
 public interface ThreadPool<Job extends Runnable>{
     //执行一个job,这个job需要实现runnable
